dece38776cf5528ecbc2f6da92df48c07d15c59d
Rise-KuN <40178259+Rise-KuN@users.noreply.github.com>
2024-10-05 03:07:19 +0100

    Update and rename theme_v1.1.4.2.js to theme_v1.1.4.3.js

--- theme_v1.1.4.3.js
+++ theme_v1.1.4.3.js
@@ -149,7 +149,7 @@ function nav(path) {
           </div>
        </div>
   `;
-  html += `<a href="/" dir=auto class="mdui-typo-headline folder">${document.siteName}</a>`;
+  html += `<div class="mdui-container"><div class="mdui-row"><a href="/" dir=auto class="mdui-typo-headline folder">${document.siteName}</a>`;
   var arr = path.trim("/").split("/");
   var p = "/";
   if (arr.length > 0) {
@@ -164,7 +164,7 @@ function nav(path) {
       if (n == "") {
         break;
       }
-      html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;transform:rotate(180deg);">chevron_right</i><a dir=auto class="folder" href="${p}">${n}</a>`;
+      html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;transform:rotate(180deg);">chevron_right</i><a dir=auto class="folder" href="${p}">${n}</a</div></div>`;
     }
   }
   $("#nav").html(html);
